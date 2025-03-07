f4a8e8e0936fb8270db7ff853b69b9c2e194f2a5
tanushka <tanushkagarg730@gmail.com>
2025-03-07 21:00:18 +0530

    Add files via upload

---
+++ Hey.java
@@ -0,0 +1,12 @@
+package javaproject;
+
+public class Hey {
+
+	public static void main(String[] args) {
+	  System.out.println("Hello");
+	  System.out.println("hrutuja");
+	  System.out.println("tanu");
+
+	}
+
+}
